27a4ae009ad639c9dc1fd36151accb45fc950824
Salvatore Laisa <salvatore.laisa@carol.health>
2026-02-15 10:57:38 +0100

    feat: adding the pages list

--- routes/index.tsx
+++ routes/index.tsx
@@ -21,11 +21,45 @@ const data = [
   },
 ];
 
+const pages = [
+  {
+    path: "/password",
+    title: "Password Generator",
+    description: "Generate secure passwords with customizable options",
+  },
+  {
+    path: "/qr-code",
+    title: "QR Code Generator",
+    description: "Generate QR codes for a given URL",
+  },
+];
+
 export default define.page(function Home() {
   return (
     <main class="index-main">
       <div class="index-container">
         <Hero isDark={false} />
+        <hr class="index-divider" />
+        <table class="index-table">
+          <thead>
+            <tr>
+              <th class="index-table-header">Page</th>
+              <th class="index-table-header">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pages.map((page) => (
+              <tr>
+                <td class="index-table-cell">
+                  <Code>
+                    <a href={page.path}>{page.title}</a>
+                  </Code>
+                </td>
+                <td class="index-table-cell">{page.description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <hr class="index-divider" />
         <table class="index-table">
           <thead>
